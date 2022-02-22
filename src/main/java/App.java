9e375fb9cb3f7f93d9376a50937eacc3cf1527ac
Vikash Basant <basantvikash360@gmail.com>
2022-02-22 17:19:08 +0530

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}
+//Name=SpringSource Tool Suite
+//        Comment=SpringSource Tool Suite
+//        Exec = /opt/sts-4.13.1.RELEASE/SpringToolSuite4
+//        Icon = /opt/sts-4.13.1.RELEASE
+//        StartupNotify=true
+//        Terminal=false
+//        Type=Application
+//        CAtegories=Development;IDE;Java;

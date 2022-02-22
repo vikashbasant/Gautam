b8532ad1f9af67db4d59c40bf8b26cf8f38b381d
Vikash Basant <basantvikash360@gmail.com>
2022-02-22 17:24:27 +0530

    Add another class Xyz

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("hello World");
+        System.out.println("Gautam");
     }
 }
 //Name=SpringSource Tool Suite
---
+++ src/main/java/Xyz.java
@@ -0,0 +1,5 @@
+public class Xyz {
+    public static void main(String[] args) {
+        System.out.println("Vikash");
+    }
+}
